8f42093e1a284afd07492ccf2621cc446f4bac1f
Yurii Matvii <tahkict.yup4ik@gmail.com>
2024-03-04 02:00:57 +0200

    refactor: scrollbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,8 +43,8 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+            Unleash the power of AI within BrainLight. Upgrade your productivity
+            with BrainLight, the open AI chat app.
           </p>
           <Button href={"/pricing"} white>
             Get Started
